58f007ccc46d470187c23fa1e9d08f4dd28cd3d1
guillesanz21 <guillermo.sanz.gonzalez@alumnos.upm.es>
2025-03-10 20:36:50 +0100

    base User model

--- backend/core/admin.py
+++ backend/core/admin.py
@@ -1,3 +1,33 @@
 from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin
+from .models import User
 
-# Register your models here.
+
+admin.site.register(User, UserAdmin)
+UserAdmin.list_display = ('email', 'username', 'is_active', 'is_email_verified', 'is_staff',)
+UserAdmin.list_filter += ('is_email_verified',)  # type: ignore
+UserAdmin.add_fieldsets = (   # Add fieldsets are used to organize the fields in the add page
+    (None, {  # None is the title of the section (not displayed)
+        'fields': ('email', 'is_email_verified',),
+    }),
+) + UserAdmin.add_fieldsets
+
+
+# @admin.register(User)
+# class UserAdmin(admin.ModelAdmin):
+#     list_display = ['email', 'username', 'is_email_verified', 'is_active', 'is_staff']
+#     search_fields = ['email', 'username']
+#     list_filter = ['is_active', 'is_email_verified', 'is_staff', 'is_superuser']
+#     ordering = ['id']
+#     readonly_fields = ['id', 'date_joined', 'last_login', 'email_hash', 'deleted_at']
+#     fieldsets = (
+#         (None, {'fields': ('id', 'email', 'username', 'password')}),
+#         ('Permissions', {'fields': ('is_active', 'is_staff', 'is_superuser')}),
+#         ('Important dates', {'fields': ('last_login', 'date_joined')}),
+#     )
+#     add_fieldsets = (
+#         (None, {
+#             'classes': ('wide',),
+#             'fields': ('email', 'username', 'password1', 'password2'),
+#         }),
+#     )
--- backend/core/models.py
+++ backend/core/models.py
@@ -1,3 +1,38 @@
+from django.contrib.auth.models import AbstractUser
 from django.db import models
 
-# Create your models here.
+
+class User(AbstractUser):
+    """Base User model"""
+    email = models.EmailField(
+        error_messages={'unique': 'A user with that email already exists.'},
+        help_text='Required. 255 characters or fewer. Letters, digits and @/./+/-/_ only.',
+        max_length=255,
+        unique=True,
+        blank=False,
+        verbose_name="email address"
+    )
+    is_email_verified = models.BooleanField(
+        help_text='Designates whether the email address has been verified.',
+        default=False,
+        verbose_name='email verified'
+    )
+    email_hash = models.CharField(
+        max_length=255,
+        blank=True,
+        null=True,
+        default=None,
+        verbose_name='email verification hash'
+    )
+    # soft delete
+    deleted_at = models.DateTimeField(
+        blank=True,
+        null=True,
+        default=None,
+        verbose_name='deleted at'
+    )
+
+    # USERNAME_FIELD - the field that we want to use as the unique identifier for the user
+    USERNAME_FIELD = 'email'
+    REQUIRED_FIELDS = ['password', 'username']
+    EMAIL_FIELD = "email"
