daa99a5caaf094f4c981ff4efb2894b2675d2268
guillesanz21 <guillermo.sanz.gonzalez@alumnos.upm.es>
2025-03-11 10:45:43 +0100

    rename core app to users app

--- backend/project/settings.py
+++ backend/project/settings.py
@@ -37,7 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'core.apps.CoreConfig',
+    'users.apps.UsersConfig',
 ]
 
 MIDDLEWARE = [
@@ -124,4 +124,4 @@ STATIC_URL = 'static/'
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 
-AUTH_USER_MODEL = 'core.User'
+AUTH_USER_MODEL = 'users.User'
